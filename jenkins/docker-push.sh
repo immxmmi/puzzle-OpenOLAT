fd37dc6330f890d2100c668c3a1e423652369a8d
immxmmi <immxmmi@gmx.at>
2025-04-14 12:20:54 +0200

    Refactor docker-push.sh: streamline tag creation and push process, improve readability

--- jenkins/docker-push.sh
+++ jenkins/docker-push.sh
@@ -16,10 +16,18 @@ if [[ -z "$IMAGE_NAME" || -z "$VERSION_TAG" || -z "$BRANCH_NAME" || -z "$REGISTR
   exit 1
 fi
 
+REGISTRY="${REGISTRY%/}"
+
 echo "$PASS" | docker login "$REGISTRY" -u "$USER" --password-stdin
 
-docker tag "${IMAGE_NAME}:${VERSION_TAG}" "${REGISTRY}/${IMAGE_NAME}:${VERSION_TAG}"
-docker tag "${IMAGE_NAME}:${VERSION_TAG}" "${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME}-latest"
+IMAGE_VERSION_TAG="${REGISTRY}/${IMAGE_NAME}:${VERSION_TAG}"
+IMAGE_LATEST_TAG="${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME}-latest"
 
-docker push "${REGISTRY}/${IMAGE_NAME}:${VERSION_TAG}"
-docker push "${REGISTRY}/${IMAGE_NAME}:${BRANCH_NAME}-latest"
+echo " - $IMAGE_VERSION_TAG"
+echo " - $IMAGE_LATEST_TAG"
+
+docker tag "${IMAGE_NAME}:${VERSION_TAG}" "$IMAGE_VERSION_TAG"
+docker tag "${IMAGE_NAME}:${VERSION_TAG}" "$IMAGE_LATEST_TAG"
+
+docker push "$IMAGE_VERSION_TAG"
+docker push "$IMAGE_LATEST_TAG"
